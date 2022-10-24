d3059c1ea65c595b12ee08b33e58fb8244ef36a0
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-10-24 18:12:34 -0300

    Correções em texto - AboutUs

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,8 +4,8 @@ const AboutUs = () => {
         <section className="aboutUs__container">
             <h2 className="aboutUs__container__title" >O que fazemos?</h2>
             <div className="aboutUs__container__text">
-                <p>O site Grandes Mentes, propoem trazer um breve resumo de quem foram as grandes mentes que já pisaram em nosso mundo, juntamente com suas frases mais marcantes em vida.</p>
-                <p>Apresentamos as frases desses pensadores de forma aleatória, pois acreditamos que a experiência da imprevisibilidade é a melhor para ponderar sobre as variadas frases desses pensadores. Contamos com uma média de 35 frases inspiradoras de cada pensador. </p>
+                <p>O site Grandes Mentes, propõem levar um breve resumo de quem foram as grandes mentes que conhecemos, juntamente com suas frases mais marcantes em vida.</p>
+                <p>Apresentamos as frases desses pensadores de forma aleatória, pois acreditamos que a experiência da imprevisibilidade é a melhor para apreciar sobre os diversos contextos em que à frase pode se encaixar. Contamos com uma média de 35 frases inspiradoras de cada pensador. </p>
             </div>
             <h2 className="aboutUs__container__title">Missão</h2>
             <div className="aboutUs__container__text">
@@ -17,7 +17,7 @@ const AboutUs = () => {
             </div>
             <h2 className="aboutUs__container__title">Valores</h2>
             <div className="aboutUs__container__text">
-                <p>Acreditamos que todo grande pensador têm sua própria visão de mundo, isso corresponde ao estilo de vida e conceitos da época em que estavam com vida, mesmo que em nossa época não concordemos com essas visões, elas ainda têm muito a nós ensinar e por isso, também têm um espaço em nosso site.</p>
+                <p>Acreditamos que todo grande pensador têm sua própria visão de mundo, isso corresponde ao estilo de vida e conceitos da época em que estavam com vida, mesmo que em nossa época não concordemos com algumas dessas visões, elas ainda têm muito à ensinar e por isso possuem um espaço em nosso site.</p>
             </div>
         </section>
     </article>
