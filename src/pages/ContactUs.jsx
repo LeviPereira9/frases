fa184aa83e5a375213445bc76f92becf030c2b38
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-10-25 18:30:48 -0300

    Atualização Index.

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,4 @@
-import { BsTwitter} from "react-icons/bs";
+import { BsTwitter, BsLinkedin} from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 
 const ContactUs = () => {
@@ -7,6 +7,7 @@ const ContactUs = () => {
         <div className="contactUs__container">
             <p className="contactUs__text" >Se tiver alguma duvida ou reclamação, entre em contato em nossas redes sociais ou e-mail:</p>
             <ul className="contactUs__links" >
+                <li><BsLinkedin/><a href="https://www.linkedin.com/in/levi-pereira-da-silva-61303a193/">Levi Pereira da Silva</a></li>
                 <li><BsTwitter/>@<a href="https://twitter.com/LeviPereira09">LeviPereira09</a></li>
                 <li> <MdEmail/> ProjetoGrandesMentes@gmail.com</li>
             </ul>
