11121555fbea124ce2119e22d939ad86a383c68f
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2022-10-25 17:51:45 -0300

    Footer Atualização 1.0.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
-import {BsGithub, BsTwitter} from "react-icons/bs"
+import {BsGithub, BsLinkedin, BsTwitter} from "react-icons/bs"
  
 const Footer = () => {
   return (
     <footer className='footer' >
         <h2 className='footer__title' >Grandes Mentes &#169; 2022 </h2>
         <ul className="footer__midias">
-          <li> <BsGithub/> <a href="https://github.com/LeviPereira9" target="_blank" >LeviPereira9</a></li>
-          <li><BsTwitter/> <a href="https://twitter.com/LeviPereira09" target="_blank" >@LeviPereira09</a></li>
+          <li>  <a href="https://github.com/LeviPereira9" target="_blank" ><BsGithub className="github"/></a></li>
+          <li>  <a href="https://www.linkedin.com/in/levi-pereira-da-silva-61303a193/"><BsLinkedin className="linkedin"/></a> </li>
+          <li>  <a href="https://twitter.com/LeviPereira09" target="_blank" ><BsTwitter className="twitter"/></a></li>
         </ul>
     </footer>
   )
